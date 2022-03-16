497cf53a72ec2c053ee69dd35516f028af61ff07
Quantum-Guru <quantumcomputersguru700@gmail.com>
2022-03-16 18:19:26 +0530

    Updates

---
+++ precache-manifest.c038dbedf08f618a404b52dc9d6c16a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e093f764fe2753f0dcffb82f52bf151",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "0efcdea749b589f5a52e",
+    "url": "/api/static/css/main.3d67e36a.chunk.css"
+  },
+  {
+    "revision": "6c3caa097e0165ae067d",
+    "url": "/api/static/js/2.74fa34e2.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/api/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "0efcdea749b589f5a52e",
+    "url": "/api/static/js/main.a8738984.chunk.js"
+  },
+  {
+    "revision": "b2c9dddaa256f5bd1d19",
+    "url": "/api/static/js/runtime~main.36484388.js"
+  }
+]);
